12d058b942b4badab08c8599989b398cfc809c9f
Darrensiriram <39519268+Darrensiriram@users.noreply.github.com>
2021-10-24 18:16:24 +0200

    Add files via upload

---
+++ LibClient/Client.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace LibClient
+{
+    // Note: Do not change this class 
+    public class Setting
+    {
+        public int ServerPortNumber { get; set; }
+        public int BookHelperPortNumber { get; set; }
+        public int UserHelperPortNumber { get; set; }
+        public string ServerIPAddress { get; set; }
+        public string BookHelperIPAddress { get; set; }
+        public string UserHelperIPAddress { get; set; }
+        public int ServerListeningQueue { get; set; }
+    }
+
+    // Note: Do not change this class 
+    public class Output
+    {
+        public string Client_id { get; set; } // the id of the client that requests the book
+        public string BookName { get; set; } // the name of the book to be reqyested
+        public string Status { get; set; } // final status received from the server
+
+        public string
+            BorrowerName { get; set; } // the name of the borrower in case the status is borrowed, otherwise null
+
+        public string
+            BorrowerEmail { get; set; } // the email of the borrower in case the status is borrowed, otherwise null
+    }
+
+    // Note: Complete the implementation of this class. You can adjust the structure of this class.
+    public class SimpleClient
+    {
+        private string bookName;
+        public string client_id;
+
+        public Socket clientSocket;
+
+        // all the required settings are provided in this file
+        public string configFile = @"../../../../ClientServerConfig.json";
+
+        public IPAddress ipAddress;
+
+        // some of the fields are defined. 
+        public Output result;
+        public IPEndPoint serverEndPoint;
+
+        public Setting settings;
+        //public string configFile = @"../../../../ClientServerConfig.json"; // for debugging
+
+        // todo: add extra fields here in case needed 
+
+        /// <summary>
+        ///     Initializes the client based on the given parameters and seeting file.
+        /// </summary>
+        /// <param name="id">id of the clients provided by the simulator</param>
+        /// <param name="bookName">name of the book to be requested from the server, provided by the simulator</param>
+        public SimpleClient(int id, string bookName)
+        {
+            //todo: extend the body if needed.
+            this.bookName = bookName;
+            client_id = "Client " + id;
+            result = new Output();
+            result.BookName = bookName;
+            result.Client_id = client_id;
+            // read JSON directly from a file
+            try
+            {
+                var configContent = File.ReadAllText(configFile);
+                settings = JsonSerializer.Deserialize<Setting>(configContent);
+                ipAddress = IPAddress.Parse(settings.ServerIPAddress);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Client Exception] {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Establishes the connection with the server and requests the book according to the specified protocol.
+        ///     Note: The signature of this method must not change.
+        /// </summary>
+        /// <returns>The result of the request</returns>
+        public Output start()
+        {
+            // todo: implement the body to communicate with the server and requests the book. Return the result as an Output object.
+            // Adding extra methods to the class is permitted. The signature of this method must not change.
+
+            return result;
+        }
+    }
+}
---
+++ LibClient/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using LibClient;
+
+
+// NOTE: DO NOT CHANGE THIS FILE
+namespace LibClientSimulator
+{
+    public class InputData
+    {
+        public string BookName { get; set; }
+    }
+
+    public class ClientsSimulator
+    {
+        private SimpleClient client;
+        private readonly SimpleClient[] clients;
+        private readonly List<InputData> inputDataList;
+        private readonly string inputFile = @"../../../LibInput.json";
+        private readonly string outputFile = @"LibOutput.json";
+        private readonly Output[] results;
+
+        // paths for debugging
+        //private string inputFile = @"../../../LibInput.json";
+        //private string outputFile = @"../../../LibOutput.json";
+
+        /// <summary>
+        ///     Reads the input file and creats the clients and output objects accordingly
+        /// </summary>
+        public ClientsSimulator()
+        {
+            var id = 0;
+            try
+            {
+                var inputContent = File.ReadAllText(inputFile);
+                inputDataList = JsonSerializer.Deserialize<List<InputData>>(inputContent);
+                clients = new SimpleClient[inputDataList.Count];
+                results = new Output[inputDataList.Count];
+
+                // each client is initialized by a client id and a book name (from the input file) to request
+                foreach (var d in inputDataList)
+                {
+                    clients[id] = new SimpleClient(id, d.BookName);
+                    id++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[ClientSimulator] Exception: {0}", e.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Starts the book requesting process for each client and collects the result for each request
+        /// </summary>
+        public void startSimulation()
+        {
+            var numCases = clients.Length;
+            for (var i = 0; i < numCases; i++)
+            {
+                Console.Out.WriteLine("\n *********** \n");
+                results[i] = clients[i].start();
+            }
+
+            //this is the ending client
+            new SimpleClient(-1, "").start();
+        }
+
+        /// <summary>
+        ///     Prints all the results in console and produces the output file
+        /// </summary>
+        public void printOutput()
+        {
+            Console.Out.WriteLine("\n ***************    Final Output       *********** \n");
+            for (var i = 0; i < results.Length; i++)
+                Console.WriteLine("{0} {1} {2} {3}",
+                    results[i].Client_id,
+                    results[i].BookName,
+                    results[i].BorrowerName,
+                    results[i].BorrowerEmail);
+            var outputContent = JsonSerializer.Serialize(results);
+            Console.WriteLine("Content of the Output file:\n {0}", outputContent);
+
+            File.WriteAllText(outputFile, outputContent);
+        }
+    }
+
+    internal class Program
+    {
+        /// <summary>
+        ///     Starts the simulation for a set of clients and produces the output results.
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            using var listener = new SocketEventListener();
+            Console.Clear();
+            var simulator = new ClientsSimulator();
+            simulator.startSimulation();
+
+            simulator.printOutput();
+        }
+
+        internal sealed class SocketEventListener : EventListener
+        {
+            // Constant necessary for attaching ActivityId to the events.
+            public const EventKeywords TasksFlowActivityIds = (EventKeywords) 0x80;
+
+            protected override void OnEventSourceCreated(EventSource eventSource)
+            {
+                // List of event source names provided by networking in .NET 5.
+                if (eventSource.Name == "System.Net.Sockets" ||
+                    eventSource.Name == "System.Net.Security" ||
+                    eventSource.Name == "System.Net.NameResolution")
+                    EnableEvents(eventSource, EventLevel.LogAlways);
+                // Turn on ActivityId.
+                else if (eventSource.Name == "System.Threading.Tasks.TplEventSource")
+                    // Attach ActivityId to the events.
+                    EnableEvents(eventSource, EventLevel.LogAlways, TasksFlowActivityIds);
+            }
+
+            protected override void OnEventWritten(EventWrittenEventArgs eventData)
+            {
+                Console.ResetColor();
+
+                var sb = new StringBuilder().Append(
+                    $"{eventData.TimeStamp:HH:mm:ss.ff}  {eventData.ActivityId}.{eventData.RelatedActivityId}  {eventData.EventSource.Name}.{eventData.EventName}(");
+                for (var i = 0; i < eventData.Payload?.Count; i++)
+                {
+                    sb.Append(eventData.PayloadNames?[i]).Append(": ").Append(eventData.Payload[i]);
+                    if (i < eventData.Payload?.Count - 1) sb.Append(", ");
+                }
+
+                sb.Append(")");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(sb.ToString());
+                Console.ResetColor();
+            }
+        }
+    }
+}
